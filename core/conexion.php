3ccb34ca1274bfca287e2b49759fbfbeaaa8ad23
David <pablodavidhuamani@gmail.com>
2023-02-28 13:08:13 -0500

    update

---
+++ core/conexion.php
@@ -0,0 +1,14 @@
+<?php
+  // Datos para la conexión
+  $server = 'localhost';
+  $user = 'ghxumdmy_yuntasproducciones';
+  $pass = 'ghxumdmy_yuntasproducciones';
+  $db = 'ghxumdmy_yuntasproducciones';
+
+  // Conexión a bd - MySQL
+  $conn = mysqli_connect($server, $user, $pass, $db);
+
+  // if(isset($conn)){
+  //   echo 'DB is connect'
+  // }
+?>
