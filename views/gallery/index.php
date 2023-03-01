13838f9683c455bd906c2a08ba016bf39b4fba4c
NicolasVS357 <125095200+NicolasVS357@users.noreply.github.com>
2023-03-01 13:39:03 -0500

    Galeria Vapingclub
    nueva pagina galeria (en proceso)

---
+++ views/gallery/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="dist/output.css" rel="stylesheet">
+  <link href="../public/css/gallery.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.7.2/dist/tailwind.min.css" />
+</head>
+<body>
+<div class="flex flex-wrap justify-center">
+  <img class="object-cover rounded-lg" id="gallerypicrow3" src="../public/combos/img2.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow3" src="../public/combos/img2.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow2" src="../public/combos/img4.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow2" src="../public/combos/img4.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow1" src="../public/combos/img5.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow1" src="../public/combos/img5.png" alt="Photo of Person">
+  <!-- more images here  -->
+</div>
+</body>
+</html>
