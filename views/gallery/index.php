a47fb0e3aaa717219bf4cdbd98c26e0841e4893e
NicolasVS357 <125095200+NicolasVS357@users.noreply.github.com>
2023-03-02 13:18:06 -0500

    Vista Galeria(in process)
    Actualización por tema de responsive

--- views/gallery/index.php
+++ views/gallery/index.php
@@ -4,17 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="dist/output.css" rel="stylesheet">
-  <link href="../public/css/gallery.css" rel="stylesheet">
+  <link href="../public/css/gallery1.css" rel="stylesheet">
    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.7.2/dist/tailwind.min.css" />
 </head>
 <body>
-<div class="flex flex-wrap justify-center">
-  <img class="object-cover rounded-lg" id="gallerypicrow3" src="../public/combos/img2.png" alt="Photo of Person">
-  <img class="object-cover rounded-lg" id="gallerypicrow3" src="../public/combos/img2.png" alt="Photo of Person">
+<div class="flex flex-wrap justify-center" id="FilaImg1">
+  <img class="object-cover rounded-lg" id="gallerypicrow1" src="../public/combos/img1.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow1" src="../public/combos/img1.png" alt="Photo of Person">
+</div>
+<div class="flex flex-wrap justify-center" id="FilaImg2">
   <img class="object-cover rounded-lg" id="gallerypicrow2" src="../public/combos/img4.png" alt="Photo of Person">
   <img class="object-cover rounded-lg" id="gallerypicrow2" src="../public/combos/img4.png" alt="Photo of Person">
-  <img class="object-cover rounded-lg" id="gallerypicrow1" src="../public/combos/img5.png" alt="Photo of Person">
-  <img class="object-cover rounded-lg" id="gallerypicrow1" src="../public/combos/img5.png" alt="Photo of Person">
+</div>
+<div class="flex flex-wrap justify-center" id="FilaImg3">
+  <img class="object-cover rounded-lg" id="gallerypicrow3" src="../public/combos/img5.png" alt="Photo of Person">
+  <img class="object-cover rounded-lg" id="gallerypicrow3" src="../public/combos/img5.png" alt="Photo of Person">
+</div>
+
   <!-- more images here  -->
 </div>
 </body>
