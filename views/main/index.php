eeadbee32234db48db596834f9291557b089ac6e
NicolasVS357 <125095200+NicolasVS357@users.noreply.github.com>
2023-03-04 12:04:17 -0500

    promociones-home
    Creando promociones home junto a  version alternativa

--- views/main/index.php
+++ views/main/index.php
@@ -4,10 +4,87 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="dist/output.css" rel="stylesheet">
+  <link href="../public/css/home.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.7.2/dist/tailwind.min.css" />
+
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+                   <!-- Producto 1 -->
+      <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div class="group relative">
+          <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h3 class="text-sm text-gray-700">
+                <div>
+                  <span aria-hidden="true" class="absolute inset-0"  id="product_title" ></span>
+                  Relx Essential X 2 Pods Pro
+                </div>
+              </h3>
+              <p class="mt-1 text-sm text-gray-500"  id="product_desc" >S/. 115.00</p><br>
+              <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-black-700 rounded">
+               <span id="btn_comprar">Comprar</span>
+              </a>             
+            </div>
+          </div>
+        </div>
+      </div>
+
+              <!-- Producto 2 -->
+      <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div class="group relative">
+          <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h3 class="text-sm text-gray-700">
+                <div>
+                  <span aria-hidden="true" class="absolute inset-0"  id="product_title" ></span>
+                  Relx Essential X 2 Pods Pro
+                </div>
+              </h3>
+              <p class="mt-1 text-sm text-gray-500"  id="product_desc" >S/. 115.00</p><br>
+              <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-black-700 rounded">
+               <span id="btn_comprar">Comprar</span>
+              </a>             
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <!-- Producto 3 -->
+
+      <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div class="group relative">
+          <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h3 class="text-sm text-gray-700">
+                <div>
+                  <span aria-hidden="true" class="absolute inset-0"  id="product_title" ></span>
+                  Relx Essential X 2 Pods Pro
+                </div>
+              </h3>
+              <p class="mt-1 text-sm text-gray-500"  id="product_desc" >S/. 115.00</p><br>
+              <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-black-700 rounded">
+               <span id="btn_comprar">Comprar</span>
+              </a>             
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+    </div>
+  </div>
 </body>
 </html>
---
+++ views/promociones_para_mainv2/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="dist/output.css" rel="stylesheet">
+  <link href="../public/css/home2.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://unpkg.com/tailwindcss@1.7.2/dist/tailwind.min.css" />
+</head>
+<body>
+     
+     <!--Producto 1-->
+    <div class="max-w-sm rounded overflow-hidden shadow-lg" id="producto1">
+        <img class="w-full" src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+    </div>
+
+         <!--Producto 2-->
+    <div class="max-w-sm rounded overflow-hidden shadow-lg" id="producto2">
+        <img class="w-full" src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+    </div>
+
+             <!--Producto 3-->
+    <div class="max-w-sm rounded overflow-hidden shadow-lg" id="producto3">
+        <img class="w-full" src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+    </div>
+</body>
+</html>
